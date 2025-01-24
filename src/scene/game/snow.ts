d56b2831a30fa09fd4683050f5577878fb301d69
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-01-24 20:59:31 +0100

    fix snow position

--- src/scene/game/snow.ts
+++ src/scene/game/snow.ts
@@ -12,8 +12,11 @@ export const letItSnow = (
   for (let x = RADIUS; x < width; x += DIAMETER) {
     for (let y = RADIUS; y < height; y += DIAMETER) {
       const snow = graphics(container)
-      snow.rect(0, 0, DIAMETER, DIAMETER).fill({ color: 'white', alpha: 1 })
-      snow.position.set(x, y)
+      // TODO: -1 just for visual comform
+      snow
+        .rect(0, 0, DIAMETER - 1, DIAMETER - 1)
+        .fill({ color: 'white', alpha: 1 })
+      snow.position.set(x - RADIUS, y - RADIUS)
       snow.zIndex = -99
     }
   }
