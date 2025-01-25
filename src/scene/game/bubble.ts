d82fa2923eda170058ef22066533d1658373b069
fabrlyn <robert.barlin@gmail.com>
2025-01-26 00:03:34 +0100

    Revert to normal condition after goes pop

--- src/scene/game/bubble.ts
+++ src/scene/game/bubble.ts
@@ -9,59 +9,60 @@ import type { AnimatedSprite } from 'pixi.js'
 const MAXIMUM_SPEED = 3
 
 const exchangeMassForSnowMass = (mass: number) => {
-  return Math.floor(mass / 10000) + 1
+    return Math.floor(mass / 10000) + 1
 }
 
 const itGoesPop = (scene: Scene, playerId: EntityId) => {
-  const snowmass = exchangeMassForSnowMass(state.masses.get(playerId)!)
+    const snowmass = exchangeMassForSnowMass(state.masses.get(playerId)!)
 
-  const s = sprites.get(playerId)! as AnimatedSprite
-  s.textures = [
-    scene.textures['popping_player_0-1'],
-    scene.textures['popping_player_0-2'],
-    scene.textures['popping_player_0-3'],
-    scene.textures['popping_player_0-4'],
-    scene.textures['popping_player_0-5'],
-  ]
-  s.play()
-  s.loop = false
-  s.onComplete = async () => {
-    setMass(playerId, MIN_MASS, scene)
+    const s = sprites.get(playerId)! as AnimatedSprite
+    s.textures = [
+        scene.textures['popping_player_0-1'],
+        scene.textures['popping_player_0-2'],
+        scene.textures['popping_player_0-3'],
+        scene.textures['popping_player_0-4'],
+        scene.textures['popping_player_0-5'],
+    ]
+    s.play()
+    s.loop = false
+    s.onComplete = async () => {
+        state.conditions.set(playerId, 'normal')
+        setMass(playerId, MIN_MASS, scene)
 
-    for (let i = 0; i < snowmass; i++) {
-      await scene.timer.delay(10)
-      growSnow()
+        for (let i = 0; i < snowmass; i++) {
+            await scene.timer.delay(10)
+            growSnow()
+        }
     }
-  }
 }
 
 export const pop = (scene: Scene, entityId: EntityId) => {
-  const condition = state.conditions.get(entityId)
-  if (!condition || condition !== 'normal') {
-    return
-  }
-
-  const entityPosition = scene.state.positions.get(entityId)!
-  const targetPosition = {
-    x: scene.app.screen.width / 2,
-    y: scene.app.screen.height / 2,
-  }
-
-  state.conditions.set(entityId, 'popping-the-bubble')
-  const direction = normalize(subtract(entityPosition, targetPosition))
-
-  const velocity = scale(MAXIMUM_SPEED, direction)
-  scene.state.velocities.set(entityId, velocity)
-
-  const unsubscribe = scene.timer.repeatEvery(1, () => {
-    const currentPosition = state.positions.get(entityId)!
-    const targetDistance = getDistance(currentPosition, targetPosition)
-
-    if (Math.abs(targetDistance) < 2) {
-      scene.state.velocities.set(entityId, { x: 0, y: 0 })
-      unsubscribe()
-      itGoesPop(scene, entityId)
-      return
+    const condition = state.conditions.get(entityId)
+    if (!condition || condition !== 'normal') {
+        return
     }
-  })
+
+    const entityPosition = scene.state.positions.get(entityId)!
+    const targetPosition = {
+        x: scene.app.screen.width / 2,
+        y: scene.app.screen.height / 2,
+    }
+
+    state.conditions.set(entityId, 'popping-the-bubble')
+    const direction = normalize(subtract(entityPosition, targetPosition))
+
+    const velocity = scale(MAXIMUM_SPEED, direction)
+    scene.state.velocities.set(entityId, velocity)
+
+    const unsubscribe = scene.timer.repeatEvery(1, () => {
+        const currentPosition = state.positions.get(entityId)!
+        const targetDistance = getDistance(currentPosition, targetPosition)
+
+        if (Math.abs(targetDistance) < 2) {
+            scene.state.velocities.set(entityId, { x: 0, y: 0 })
+            unsubscribe()
+            itGoesPop(scene, entityId)
+            return
+        }
+    })
 }
