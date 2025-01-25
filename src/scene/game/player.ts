456a4a54bdd56a0c1364c2165be685448a903ae1
Simon Lagos <simon.lagos.sallhed@gmail.com>
2025-01-25 15:35:02 +0100

    Min radious

--- src/scene/game/player.ts
+++ src/scene/game/player.ts
@@ -4,12 +4,14 @@ import { sprites, state, type EntityId, type Radius } from '~/data'
 const SPRITE_SCALE_FACTOR = 0.055
 
 export const setRadius = (playerId: EntityId, radius: Radius) => {
+  const cappedRadius = Math.max(radius, 5)
+
   // const radius = Math.sqrt(mass)
-  const spriteScale = radius * SPRITE_SCALE_FACTOR
-  const mass = radius
+  const spriteScale = cappedRadius * SPRITE_SCALE_FACTOR
+  const mass = cappedRadius
 
   state.masses.set(playerId, mass)
-  state.radii.set(playerId, radius)
+  state.radii.set(playerId, cappedRadius)
   const playerSprite = sprites.get(playerId)!
   playerSprite.scale = spriteScale
 }
