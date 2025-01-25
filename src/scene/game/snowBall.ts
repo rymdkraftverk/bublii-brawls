2f40268a06125de7542f49e1a3fc70044ba88852
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-01-25 12:12:17 +0100

    scale snowball/player size at launch

--- src/scene/game/snowBall.ts
+++ src/scene/game/snowBall.ts
@@ -2,23 +2,42 @@ import { sprite } from 'alchemy-engine'
 import { getNextId, sprites, state, type EntityId } from '~/data'
 import type { Scene } from '~/type'
 
-const SPEED = 3 // CONFIG
+// CONFIG
+const SPEED = 3
+const PLAYER_MASS_REQUIREMENT = 1
+
+const SNOWBALL_FACTOR = .1
+const PLAYER_SHRINK_FACTOR = 1 - SNOWBALL_FACTOR
 
 export const launch = (scene: Scene, from: EntityId, angleRadian: number) => {
+  const playerMass = state.masses.get(from)!
+  if (playerMass < PLAYER_MASS_REQUIREMENT) return
+
+  // create snowball
+  const id = getNextId()
+
   const velocity = {
     x: SPEED * Math.cos(angleRadian),
     y: SPEED * Math.sin(angleRadian),
   }
 
-  const id = getNextId()
-
+  const snowBallMass = playerMass * SNOWBALL_FACTOR
   const fromPosition = state.positions.get(from)!
   state.positions.set(id, fromPosition)
   state.velocities.set(id, velocity)
+  state.radii.set(id, snowBallMass)
 
   const s = sprite(scene.container)
   s.texture = scene.textures['snowball_0-1']
   s.position.set(fromPosition.x, fromPosition.y)
-
+  s.scale.set(snowBallMass)
   sprites.set(id, s)
+
+  // shrink player
+  const shrunkPlayerMass = playerMass * PLAYER_SHRINK_FACTOR
+  state.masses.set(from, shrunkPlayerMass)
+  state.radii.set(from, shrunkPlayerMass)
+  const playerSprite = sprites.get(from)!
+  const playerScale = playerSprite.scale.x
+  playerSprite.scale = playerScale * PLAYER_SHRINK_FACTOR
 }
