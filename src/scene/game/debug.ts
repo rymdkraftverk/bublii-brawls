7b79256f76a4c261a08c1e26f11efb10d9061424
Lorentz Lasson <lasson.lorentz@gmail.com>
2025-01-26 15:26:29 +0100

    satisfy CI

--- src/scene/game/debug.ts
+++ src/scene/game/debug.ts
@@ -1,26 +1,26 @@
 import { graphics } from 'alchemy-engine'
-import type { Type } from '~/data'
+// import type { Type } from '~/data'
 import type { Scene } from '~/type'
 
 export default function debug(scene: Scene) {
   const debugGraphics = graphics(scene.container)
   debugGraphics.zIndex = 99999
 
-  function debugType(type: Type) {
-    for (const id of scene.state.typeToIds[type]) {
-      const position = scene.state.positions.get(id)
-      const radius = scene.state.radii.get(id)
-
-      if (!position || !radius) {
-        console.warn(`Trying to debug "${type}" without position or radius`)
-        return
-      }
-
-      debugGraphics
-        .circle(position.x, position.y, radius)
-        .stroke({ width: 1, color: 'black' })
-    }
-  }
+  // function debugType(type: Type) {
+  //   for (const id of scene.state.typeToIds[type]) {
+  //     const position = scene.state.positions.get(id)
+  //     const radius = scene.state.radii.get(id)
+  //
+  //     if (!position || !radius) {
+  //       console.warn(`Trying to debug "${type}" without position or radius`)
+  //       return
+  //     }
+  //
+  //     debugGraphics
+  //       .circle(position.x, position.y, radius)
+  //       .stroke({ width: 1, color: 'black' })
+  //   }
+  // }
 
   scene.timer.repeatEvery(1, () => {
     debugGraphics.clear()
